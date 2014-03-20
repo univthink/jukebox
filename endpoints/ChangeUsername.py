57ca012d387404294bcfebda8455533a322ac25d
Evan Plotkin <evanplotkin@rescomp-13-269990.stanford.edu>
2014-03-20 04:11:01 -0700

    Fixed bugs in ChangeUsername

--- endpoints/ChangeUsername.py
+++ endpoints/ChangeUsername.py
@@ -6,16 +6,23 @@ class ChangeUsername(webapp2.RequestHandler):
 	def post(self):
 		self.response.headers['Content-Type'] = 'application/json'
 
-		user = utils.get_user_by_id(self.request.get('user_id'))
+		try:
+			user = utils.get_user_by_id(self.request.get('user_id'))
+		except:
+			self.response.write(json.dumps({"status":"NOT OK", "message":"The requested user was not found."}))
+			return
 
 		if not user:
 			self.response.write(json.dumps({"status":"NOT OK", "message":"The requested user was not found."}))
+			return
 
 		if len(self.request.get('name','')) == 0:
 			self.response.write(json.dumps({"status":"NOT OK","message":"You must include a valid, new username."}))
+			return
 
 		if len(self.request.get('name','')) > 20:
 			self.response.write(json.dumps({"status":"NOT OK","message":"Usernames cannot be greater than 20 characters."}))
+			return
 
 		user.username = self.request.get('name')
 		user.put()
