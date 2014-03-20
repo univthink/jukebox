410fe226b246b7ae594697c14d74048764270672
Evan Plotkin <evanplotkin@rescomp-13-269990.stanford.edu>
2014-03-19 21:54:48 -0700

    Changed ChangeUsername to use json.dumps

--- endpoints/ChangeUsername.py
+++ endpoints/ChangeUsername.py
@@ -9,12 +9,15 @@ class ChangeUsername(webapp2.RequestHandler):
 		user = utils.get_user_by_id(self.request.get('user_id'))
 
 		if not user:
-			self.response.write({"status":"NOT OK", "message":"The requested user was not found."})
+			self.response.write(json.dumps({"status":"NOT OK", "message":"The requested user was not found."}))
 
 		if len(self.request.get('name','')) == 0:
-			self.response.write({"status":"NOT OK","message":"You must include a valid, new username."})
+			self.response.write(json.dumps({"status":"NOT OK","message":"You must include a valid, new username."}))
+
+		if len(self.request.get('name','')) > 20:
+			self.response.write(json.dumps({"status":"NOT OK","message":"Usernames cannot be greater than 20 characters."}))
 
 		user.username = self.request.get('name')
 		user.put()
 
-		self.response.write({"status":"OK"})
+		self.response.write(json.dumps({"status":"OK"}))
