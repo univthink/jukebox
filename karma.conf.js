5d00ccb85e41a4cd7a67bb96f0d162e3d6601c93
Kyle Dumovic <duma@flux.io>
2016-02-05 13:56:54 -0800

    Revert "resolving merge conflict"
    This reverts commit 4e02a099a460c2a63ce6bda91d68541884145bef, reversing
    changes made to 632a525f93014ba978091bbdad1c41530e74cc4c.

--- karma.conf.js
+++ karma.conf.js
@@ -1,71 +1,33 @@
+module.exports = function(config){
+  config.set({
 
-module.exports = function ( karma ) {
-  process.env.PHANTOMJS_BIN = 'node_modules/karma-phantomjs-launcher/node_modules/.bin/phantomjs';
+    basePath : './',
 
-  karma.set({
-    /**
-     * From where to look for files, starting with the location of this file.
-     */
-    basePath: './',
+    files : [
+      'app/bower_components/angular/angular.js',
+      'app/bower_components/angular-route/angular-route.js',
+      'app/bower_components/angular-mocks/angular-mocks.js',
+      'app/components/**/*.js',
+      'app/view*/**/*.js'
+    ],
 
-    /**
-     * Filled by the task `gulp karma-conf`
-     */
-    files: [
-                    'bower_components/angular/angular.js',
-                    'bower_components/angular-route/angular-route.js',
-                    'bower_components/angular-mocks/angular-mocks.js',
-                    'src/app/app.js',
-                    'src/app/common/footer/footer.js',
-                    'src/app/todo/todo.js',
-                    'src/app/todo/todo-controller.js',
-                    'src/app/songs/songsController.js',
-                    'src/app/songs/songInfoController.js',
-                    '.tmp/jukebox-templates.js',
-                    'src/app/todo/todo-controller_test.js'
-                  ],
+    autoWatch : true,
 
-    frameworks: [ 'mocha', 'chai' ],
-    plugins: [ 'karma-mocha', 'karma-chai', 'karma-phantomjs-launcher' ],
+    frameworks: ['jasmine'],
 
-    /**
-     * How to report, by default.
-     */
-    reporters: 'progress',
+    browsers : ['Chrome'],
 
-    /**
-     * Show colors in output?
-     */
-    colors: true,
+    plugins : [
+            'karma-chrome-launcher',
+            'karma-firefox-launcher',
+            'karma-jasmine',
+            'karma-junit-reporter'
+            ],
 
-    /**
-     * On which port should the browser connect, on which port is the test runner
-     * operating, and what is the URL path for the browser to use.
-     */
-    port: 9099,
-    runnerPort: 9100,
-    urlRoot: '/',
+    junitReporter : {
+      outputFile: 'test_out/unit.xml',
+      suite: 'unit'
+    }
 
-    /**
-     * Disable file watching by default.
-     */
-    autoWatch: false,
-
-    /**
-     * The list of browsers to launch to test on. This includes only "Firefox" by
-     * default, but other browser names include:
-     * Chrome, ChromeCanary, Firefox, Opera, Safari, PhantomJS
-     *
-     * Note that you can also use the executable name of the browser, like "chromium"
-     * or "firefox", but that these vary based on your operating system.
-     *
-     * You may also leave this blank and manually navigate your browser to
-     * http://localhost:9099/ when you're running tests. The window/tab can be left
-     * open and the tests will automatically occur there during the build. This has
-     * the aesthetic advantage of not launching a browser every time you save.
-     */
-    browsers: [
-      'PhantomJS'
-    ]
   });
 };
