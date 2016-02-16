d745786e1da412c36f0b4ebcd2e73f20238c5a72
Kyle Dumovic <duma@flux.io>
2016-02-15 23:12:45 -0800

    Updated dependencies

--- src/app/queue/queueController.js
+++ src/app/queue/queueController.js
@@ -4,58 +4,59 @@
 
   angular
     .module('jukebox')
-    .controller('QueueController', function($scope, $routeParams, $modal, backendAPI, sharedRoomData) {
+    .controller('QueueController',
+      function($scope, $routeParams, $uibModal, backendAPI, sharedRoomData) {
 
-      $scope.status = '';
-      $scope.queueData = {};
+        $scope.status = '';
+        $scope.queueData = {};
 
-      sharedRoomData.roomId = $routeParams.roomId;
-      $scope.roomId = sharedRoomData.roomId;
+        sharedRoomData.roomId = $routeParams.roomId;
+        $scope.roomId = sharedRoomData.roomId;
 
-      sharedRoomData.password = ''; // TODO: update when we ask user
-      sharedRoomData.userId = '5629499534213120';
-      sharedRoomData.userName = 'kyle';
+        sharedRoomData.password = ''; // TODO: update when we ask user
+        sharedRoomData.userId = '5629499534213120';
+        sharedRoomData.userName = 'kyle';
 
-      // Example usage of backendAPI factory
-      function joinRoom() {
-        backendAPI.joinRoom({
-          room_id: sharedRoomData.roomId,
-          user_id: sharedRoomData.userId,
-          password: sharedRoomData.password,
-        }).success(function(data) {
-          console.log(data);
-          if (data.status === 'NOT OK') {
-            $scope.status = data.message;
-          } else {
-            console.log('Join room was a success!');
+        // Example usage of backendAPI factory
+        function joinRoom() {
+          backendAPI.joinRoom({
+            room_id: sharedRoomData.roomId,
+            user_id: sharedRoomData.userId,
+            password: sharedRoomData.password,
+          }).success(function(data) {
             console.log(data);
-          }
-        }).error(function(error) {
-          $scope.status = error.message;
-        });
-      }
+            if (data.status === 'NOT OK') {
+              $scope.status = data.message;
+            } else {
+              console.log('Join room was a success!');
+              console.log(data);
+            }
+          }).error(function(error) {
+            $scope.status = error.message;
+          });
+        }
 
-      // joinRoom();
+        // joinRoom();
 
-      function getSongQueue() {
-        console.log(sharedRoomData.roomId);
-        backendAPI.getSongQueue({
-          room_id: '5066549580791808',
-          password: sharedRoomData.password,
-        }).success(function(data) {
-          if (data.status === 'OK') {
-            sharedRoomData.roomName = data.room_name;
-            $scope.queueData = data.data;
-            console.log(data.data);
-          } else {
-            console.log('Error', data);
-          }
-        }).error(function(error) {
-          console.log(error);
-        });
-      }
+        function getSongQueue() {
+          console.log(sharedRoomData.roomId);
+          backendAPI.getSongQueue({
+            room_id: '5066549580791808',
+            password: sharedRoomData.password,
+          }).success(function(data) {
+            if (data.status === 'OK') {
+              sharedRoomData.roomName = data.room_name;
+              $scope.queueData = data.data;
+              console.log(data.data);
+            } else {
+              console.log('Error', data);
+            }
+          }).error(function(error) {
+            console.log(error);
+          });
+        }
 
-      getSongQueue();
+        getSongQueue();
 
   });
 
