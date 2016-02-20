50fb24096234a49cbd7caa825fb847bb38caff46
Kyle Dumovic <duma@flux.io>
2016-02-19 16:04:43 -0800

    Prep for deployment.

--- dist/jukebox.js
+++ dist/jukebox.js
@@ -106,26 +106,6 @@
 })();
 (function () {
 
-  'use strict';
-
-  angular
-    .module('jukebox')
-    .directive('footer', footer);
-
-  function footer() {
-    return {
-      restrict: 'A',
-      replace: true,
-      scope: {
-
-      },
-      templateUrl: 'common/footer/footer.html'
-    };
-  }
-
-})();
-(function () {
-
     'use strict';
 
     angular
@@ -284,6 +264,21 @@
             return $http.post(urlBase + '/delete_song', data);
         };
 
+        /* Search for rooms
+         *
+         * data = {
+         *   coordinates: String,   (optional)  // e.g. '37.4243622,-122.1472004'
+         *   distance: String,      (optional)  // defaults to '1000'
+         *   member_id: String,     (optional)
+         *   name: String,          (optional)
+         *   creator_id: String,    (optional)
+         * }
+         *
+         */
+        backendAPI.searchRooms = function(data) {
+            return $http.post(urlBase + '/search_room', data);
+        };
+
         return backendAPI;
     }]);
 
@@ -294,158 +289,18 @@
 
   angular
     .module('jukebox')
-    .controller('QueueController', function($scope, $routeParams, $cookies, backendAPI, sharedRoomData) {
-      $scope.pageClass = 'queue-page';
+    .directive('footer', footer);
 
-      $scope.room = sharedRoomData;
-      sharedRoomData.roomId = $routeParams.roomId;
-      $scope.roomId = sharedRoomData.roomId; //TODO(kyle): Remove this after plusButton.directive.js has been updated by justin
+  function footer() {
+    return {
+      restrict: 'A',
+      replace: true,
+      scope: {
 
-      // TODO: move this elsewhere
-      var POTENTIAL_USERNAMES = ['banana', 'apple', 'peach', 'mango', 'cherry', 'grape', 'pear', 'plum', 'pineapple', 'kiwi'];
-
-      sharedRoomData.userId = $cookies.get('jb_user_id'); // TODO: think about moving this to sharedRoomData factory initialization
-      sharedRoomData.userName = $cookies.get('jb_user_name');
-      sharedRoomData.password = $cookies.get(sharedRoomData.roomId) ? $cookies.get(sharedRoomData.roomId) : '';
-
-      if (!sharedRoomData.userId || !sharedRoomData.userName) {
-        sharedRoomData.userName = POTENTIAL_USERNAMES[Math.floor(Math.random()*POTENTIAL_USERNAMES.length)];
-        console.log("Your username will be", sharedRoomData.userName);
-        createUser(sharedRoomData.userName);
-      } else {
-        console.log("You already have a username! It is", sharedRoomData.userName);
-        joinRoom(); // join room if you haven't already
-      }
-
-      function createUser(name) {
-        backendAPI.registerUser({
-          username: name,
-        }).success(function(data) {
-          if (data.status === 'OK') {
-            sharedRoomData.userId = data.data;
-            $cookies.put('jb_user_name', sharedRoomData.userName);
-            $cookies.put('jb_user_id', sharedRoomData.userId);
-            joinRoom();
-            console.log('OK backendAPI.registerUser', data);
-          } else {
-            console.log('NOT OK backendAPI.registerUser', data);
-          }
-        }).error(function(error) {
-          console.log('ERROR backendAPI.registerUser', error);
-        })
-      }
-
-      // TODO: All of these should probably be moved to a separate service...
-      function joinRoom() {
-        backendAPI.joinRoom({
-          room_id: sharedRoomData.roomId,
-          user_id: sharedRoomData.userId,
-          password: sharedRoomData.password,
-        }).success(function(data) {
-          if (data.status === 'OK') {
-            console.log('OK backendAPI.joinRoom', data);
-            getSongQueue();
-          } else {
-            if (data.message == "The correct password was not provided.") { // TODO: reuse this code below?
-              sharedRoomData.passwordProtected = true;
-              sharedRoomData.password = window.prompt("Enter the password:");
-              $cookies.put(sharedRoomData.roomId, sharedRoomData.password);
-              joinRoom();
-            }
-            console.log('NOT OK backendAPI.joinRoom', data);
-          }
-        }).error(function(error) {
-          console.log('ERROR backendAPI.joinRoom', error);
-        });
-      }
-
-      function getSongQueue() {
-        backendAPI.getSongQueue({
-          room_id: sharedRoomData.roomId,
-          password: sharedRoomData.password,
-        }).success(function(data) {
-          if (data.status === 'OK') {
-            sharedRoomData.roomName = data.room_name;
-            sharedRoomData.queue = data.data;
-            console.log('OK backendAPI.getSongQueue', data);
-          } else {
-            if (data.message == "The correct password was not provided.") {
-              sharedRoomData.passwordProtected = true;
-              sharedRoomData.password = window.prompt("Enter the password:");
-              $cookies.put(sharedRoomData.roomId, sharedRoomData.password);
-              getSongQueue();
-            }
-            console.log('NOT OK backendAPI.getSongQueue', data);
-          }
-        }).error(function(error) {
-          console.log('ERROR backendAPI.getSongQueue', error);
-        });
-      }
-
-      function changeSongPosition(songId, newPos) {
-        backendAPI.reorderSong({
-          room_id: sharedRoomData.roomId,
-          user_id: sharedRoomData.userId,
-          password: sharedRoomData.password,
-          song_id: songId,
-          new_pos: newPos,
-        }).success(function(data) {
-          if (data.status === 'OK') {
-            console.log('OK backendAPI.reorderSong', data);
-            getSongQueue();
-          } else {
-            console.log('NOT OK backendAPI.reorderSong', data);
-          }
-        }).error(function(data) {
-          console.log('ERROR backendAPI.reorderSong', error);
-        });
-      }
-
-      function deleteSong(songId) {
-        backendAPI.deleteSong({
-          room_id: sharedRoomData.roomId,
-          user_id: sharedRoomData.userId,
-          password: sharedRoomData.password,
-          song_id: songId,
-        }).success(function(data) {
-          if (data.status === 'OK') {
-            console.log('OK backendAPI.deleteSong', data);
-            getSongQueue();
-          } else {
-            console.log('NOT OK backendAPI.deleteSong', data);
-          }
-        }).error(function(data) {
-          console.log('ERROR backendAPI.deleteSong', error);
-        });
-      }
-
-      // TODO: only allow if admin
-      $scope.dragControlListeners = {
-        accept: function (sourceItemHandleScope, destSortableScope) {
-          return true; //override to determine drag is allowed or not. default is true.
-        },
-        itemMoved: function (event) {},
-        orderChanged: function(event) {
-          var songId = event.source.itemScope.element[0].dataset.uuid;
-          var newPos = event.dest.index;
-          changeSongPosition(songId, newPos);
-        },
-        //containment: '#board', //optional param.
-        //clone: true, //optional param for clone feature.
-        //allowDuplicates: false, //optional param allows duplicates to be dropped.
-      };
-
-      // $scope.dragControlListeners1 = {
-      //   //containment: '#board', //optional param.
-      //   allowDuplicates: true, //optional param allows duplicates to be dropped.
-      // };
-
-      $scope.deleteSong = function($event) {
-        var songId = $event.target.parentElement.dataset.uuid; // another, slower, way: angular.element($event.target).parent()
-        deleteSong(songId);
-      }
-
-  });
+      },
+      templateUrl: 'common/footer/footer.html'
+    };
+  }
 
 })();
 (function () {
@@ -573,10 +428,221 @@
 
   angular
     .module('jukebox')
+    .controller('QueueController', function($scope, $routeParams, $cookies, backendAPI, sharedRoomData) {
+      $scope.pageClass = 'queue-page';
+
+      $scope.room = sharedRoomData;
+      sharedRoomData.roomId = $routeParams.roomId;
+      $scope.roomId = sharedRoomData.roomId; //TODO(kyle): Remove this after plusButton.directive.js has been updated by justin
+
+      // TODO: move this elsewhere
+      var POTENTIAL_USERNAMES = ['banana', 'apple', 'peach', 'mango', 'cherry', 'grape', 'pear', 'plum', 'pineapple', 'kiwi'];
+
+      sharedRoomData.userId = $cookies.get('jb_user_id'); // TODO: think about moving this to sharedRoomData factory initialization
+      sharedRoomData.userName = $cookies.get('jb_user_name');
+      sharedRoomData.password = $cookies.get(sharedRoomData.roomId) ? $cookies.get(sharedRoomData.roomId) : '';
+
+      if (!sharedRoomData.userId || !sharedRoomData.userName) {
+        sharedRoomData.userName = POTENTIAL_USERNAMES[Math.floor(Math.random()*POTENTIAL_USERNAMES.length)];
+        console.log("Your username will be", sharedRoomData.userName);
+        createUser(sharedRoomData.userName);
+      } else {
+        console.log("You already have a username! It is", sharedRoomData.userName);
+        joinRoom(); // join room if you haven't already
+      }
+
+      function createUser(name) {
+        backendAPI.registerUser({
+          username: name,
+        }).success(function(data) {
+          if (data.status === 'OK') {
+            sharedRoomData.userId = data.data;
+            $cookies.put('jb_user_name', sharedRoomData.userName);
+            $cookies.put('jb_user_id', sharedRoomData.userId);
+            joinRoom();
+            console.log('OK backendAPI.registerUser', data);
+          } else {
+            console.log('NOT OK backendAPI.registerUser', data);
+          }
+        }).error(function(error) {
+          console.log('ERROR backendAPI.registerUser', error);
+        });
+      }
+
+      // TODO: All of these should probably be moved to a separate service...
+      function joinRoom() {
+        backendAPI.joinRoom({
+          room_id: sharedRoomData.roomId,
+          user_id: sharedRoomData.userId,
+          password: sharedRoomData.password,
+        }).success(function(data) {
+          if (data.status === 'OK') {
+            console.log('OK backendAPI.joinRoom', data);
+            getSongQueue();
+          } else {
+            if (data.message == "The correct password was not provided.") { // TODO: reuse this code below?
+              sharedRoomData.passwordProtected = true;
+              sharedRoomData.password = window.prompt("Enter the password:");
+              $cookies.put(sharedRoomData.roomId, sharedRoomData.password);
+              joinRoom();
+            }
+            console.log('NOT OK backendAPI.joinRoom', data);
+          }
+        }).error(function(error) {
+          console.log('ERROR backendAPI.joinRoom', error);
+        });
+      }
+
+      function getSongQueue() {
+        backendAPI.getSongQueue({
+          room_id: sharedRoomData.roomId,
+          password: sharedRoomData.password,
+        }).success(function(data) {
+          if (data.status === 'OK') {
+            sharedRoomData.roomName = data.room_name;
+            sharedRoomData.queue = data.data;
+            console.log('OK backendAPI.getSongQueue', data);
+          } else {
+            if (data.message == "The correct password was not provided.") {
+              sharedRoomData.passwordProtected = true;
+              sharedRoomData.password = window.prompt("Enter the password:");
+              $cookies.put(sharedRoomData.roomId, sharedRoomData.password);
+              getSongQueue();
+            }
+            console.log('NOT OK backendAPI.getSongQueue', data);
+          }
+        }).error(function(error) {
+          console.log('ERROR backendAPI.getSongQueue', error);
+        });
+      }
+
+      function changeSongPosition(songId, newPos) {
+        backendAPI.reorderSong({
+          room_id: sharedRoomData.roomId,
+          user_id: sharedRoomData.userId,
+          password: sharedRoomData.password,
+          song_id: songId,
+          new_pos: newPos,
+        }).success(function(data) {
+          if (data.status === 'OK') {
+            console.log('OK backendAPI.reorderSong', data);
+            getSongQueue();
+          } else {
+            console.log('NOT OK backendAPI.reorderSong', data);
+          }
+        }).error(function(data) {
+          console.log('ERROR backendAPI.reorderSong', error);
+        });
+      }
+
+      function deleteSong(songId) {
+        backendAPI.deleteSong({
+          room_id: sharedRoomData.roomId,
+          user_id: sharedRoomData.userId,
+          password: sharedRoomData.password,
+          song_id: songId,
+        }).success(function(data) {
+          if (data.status === 'OK') {
+            console.log('OK backendAPI.deleteSong', data);
+            getSongQueue();
+          } else {
+            console.log('NOT OK backendAPI.deleteSong', data);
+          }
+        }).error(function(data) {
+          console.log('ERROR backendAPI.deleteSong', error);
+        });
+      }
+
+      // TODO: only allow if admin
+      $scope.dragControlListeners = {
+        accept: function (sourceItemHandleScope, destSortableScope) {
+          return true; //override to determine drag is allowed or not. default is true.
+        },
+        itemMoved: function (event) {},
+        orderChanged: function(event) {
+          var songId = event.source.itemScope.element[0].dataset.uuid;
+          var newPos = event.dest.index;
+          changeSongPosition(songId, newPos);
+        },
+        //containment: '#board', //optional param.
+        //clone: true, //optional param for clone feature.
+        //allowDuplicates: false, //optional param allows duplicates to be dropped.
+      };
+
+      // $scope.dragControlListeners1 = {
+      //   //containment: '#board', //optional param.
+      //   allowDuplicates: true, //optional param allows duplicates to be dropped.
+      // };
+
+      $scope.deleteSong = function($event) {
+        var songId = $event.target.parentElement.dataset.uuid; // another, slower, way: angular.element($event.target).parent()
+        deleteSong(songId);
+      }
+
+  });
+
+})();
+(function () {
+
+  'use strict';
+
+  angular
+    .module('jukebox')
     .controller('HomeController', homeController);
 
-  function homeController($scope) {
+  function homeController($scope, $location, backendAPI) {
+
+    var GEOLOCATION_TIMEOUT = 30; // in seconds
+    var OLDEST_CACHED_GEOLOCATION_TO_ACCEPT = 60; // in seconds
+
     $scope.pageClass = 'home-page';
+    $scope.coordinates = [];
+
+    // $scope.logoFill = 'black';
+
+    function getLocation(callbackOnSuccess, callbackOnFailure) {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          function(position) {
+            $scope.logoFill = 'url(#lg)';
+            console.log('Location acquired.');
+            var lat = position.coords.latitude;
+            var lng = position.coords.longitude;
+            $scope.coordinates = [lat, lng];
+            console.log($scope.coordinates);
+            if (callbackOnSuccess) callbackOnSuccess();
+          },
+          function() {
+            if (callbackOnFailure) callbackOnFailure();
+          },
+          { timeout: GEOLOCATION_TIMEOUT * 1000 },
+          { maximumAge: OLDEST_CACHED_GEOLOCATION_TO_ACCEPT * 1000 } // I'm not sure if this is helpful
+        );
+      } else {
+        if (callbackOnFailure) callbackOnFailure(); // browser/device does not support geolocation
+      }
+    }
+
+    getLocation(
+      function() { // success
+        backendAPI.searchRooms({
+          coordinates: $scope.coordinates[0] + ',' + $scope.coordinates[1],
+          distance: '10000'
+        }).success(function(data) {
+          if (data.status === 'OK') {
+            console.log('OK backendAPI.searchRooms', data);
+            $location.path(data.data[0].id);
+          } else {
+            console.log('NOT OK backendAPI.searchRooms', data);
+          }
+        }).error(function(data) {
+          console.log('NOT OK backendAPI.searchRooms', data);
+        });
+      },
+      function() { // failure
+        console.log("Could not get location.");
+      });
+
   }
 
 })();
@@ -588,7 +654,7 @@ try {
 }
 module.run(['$templateCache', function($templateCache) {
   $templateCache.put('/jukebox/home/home.html',
-    '<div class="home-page"><div class="jb-icon"><svg><use xlink:href="#plus-icon"></use></svg></div></div>');
+    '<div class="home-page"><div class="jb-icon"><svg><lineargradient id="lg" x1="1" y1="1" x2="0" y2="0"><stop offset="0%" stop-opacity="1" stop-color="white"><stop offset="0%" stop-opacity="1" stop-color="white"><animate attributename="offset" values="0;1" fill="freeze" repeatcount="0" dur="5s" begin="0s"></stop><stop offset="0%" stop-opacity="1" stop-color="black"><animate attributename="offset" values="0;1" fill="freeze" repeatcount="0" dur="5s" begin="0s"></stop><stop offset="100%" stop-opacity="1" stop-color="black"></lineargradient><use xlink:href="#plus-icon"></use></svg></div></div>');
 }]);
 })();
 
@@ -623,8 +689,8 @@ try {
   module = angular.module('jukebox', []);
 }
 module.run(['$templateCache', function($templateCache) {
-  $templateCache.put('/jukebox/common/header/header.html',
-    '<div class="header"><div class="header-info"><div class="header-label song"><span>{{ currentlyPlaying.track }}</span></div><div class="header-label artist"><span>{{ currentlyPlaying.artist }}</span></div></div><div class="record-player"><div class="record" ng-if="currentlyPlaying"><img src="{{ currentlyPlaying.image_url }}"></div></div></div>');
+  $templateCache.put('/jukebox/common/footer/footer.html',
+    '<div class="footer"><div class="footer-label"><span>Jukebox App</span></div><div class="footer-copyright"><span>&copy; 2016, All rights reserved.</span></div></div>');
 }]);
 })();
 
@@ -635,8 +701,8 @@ try {
   module = angular.module('jukebox', []);
 }
 module.run(['$templateCache', function($templateCache) {
-  $templateCache.put('/jukebox/common/footer/footer.html',
-    '<div class="footer"><div class="footer-label"><span>Jukebox App</span></div><div class="footer-copyright"><span>&copy; 2016, All rights reserved.</span></div></div>');
+  $templateCache.put('/jukebox/common/header/header.html',
+    '<div class="header"><div class="header-info"><div class="header-label song"><span>{{ currentlyPlaying.track }}</span></div><div class="header-label artist"><span>{{ currentlyPlaying.artist }}</span></div></div><div class="record-player"><div class="record" ng-if="currentlyPlaying"><img src="{{ currentlyPlaying.image_url }}"></div></div></div>');
 }]);
 })();
 
